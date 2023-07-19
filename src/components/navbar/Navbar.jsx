919903d29ddf70171f9488cb3d3f0de5fecc9e2f
bubble003 <adiworkstoo@gmail.com>
2023-07-20 00:57:25 +0530

    commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,8 @@
-"use client"
+"use client";
 import Link from "next/link";
 import React from "react";
 import styles from "./page.module.css";
+import DarkModeToggle from "../DarkModeToggle/DarkModeToggle";
 
 const Navbar = () => {
   const links = [
@@ -44,6 +45,7 @@ const Navbar = () => {
         XOXO{" "}
       </Link>
       <div className={styles.links}>
+        <DarkModeToggle />
         {links.map((link) => (
           <Link key={link.id} href={link.url} className={styles.link}>
             {link.title}
