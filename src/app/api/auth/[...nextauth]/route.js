f727e3d5483f2ee4752783264ad5d72e9ac90877
bubble003 <adiworkstoo@gmail.com>
2023-07-21 21:07:56 +0530

    improved css

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -7,23 +7,20 @@ import bcrypt from "bcryptjs";
 
 const handler = NextAuth({
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-
     CredentialsProvider({
       id: "credentials",
       name: "Credentials",
-
       async authorize(credentials) {
+        //Check if the user exists.
         await connect();
 
         try {
-          const user = User.findOne({ email: credentials.email });
+          const user = await User.findOne({
+            email: credentials.email,
+          });
 
           if (user) {
-            const isPasswordCorrect = await bcrypt(
+            const isPasswordCorrect = await bcrypt.compare(
               credentials.password,
               user.password
             );
@@ -31,17 +28,24 @@ const handler = NextAuth({
             if (isPasswordCorrect) {
               return user;
             } else {
-              throw new Error("wrong Credentials");
+              throw new Error("Wrong Credentials!");
             }
           } else {
-            throw new Error("User not found");
+            throw new Error("User not found!");
           }
         } catch (err) {
           throw new Error(err);
         }
       },
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
   ],
+  pages: {
+    error: "/dashboard/login",
+  },
 });
 
 export { handler as GET, handler as POST };
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -14,11 +14,11 @@ const Dashboard = () => {
 
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-  const { data, mutate,error, isLoading } = useSWR(
+  const { data, mutate, error, isLoading } = useSWR(
     `/api/posts?username=${session?.data?.user.name}`,
     fetcher
   );
-    console.log(data);
+  console.log(data);
   if (session.status === "loading") {
     return <p>Loading...</p>;
   }
@@ -51,44 +51,65 @@ const Dashboard = () => {
         }),
       });
       mutate();
-      e.target.reset()
+      e.target.reset();
     } catch (err) {
       console.log(err);
     }
   };
 
- const handleDelete = async (id) => {
-   try {
-     await fetch(`/api/posts/${id}`, {
-       method: "DELETE",
-     });
-     mutate();
-   } catch (err) {
-     console.log(err);
-   }
- };
+  const handleDelete = async (id) => {
+    try {
+      await fetch(`/api/posts/${id}`, {
+        method: "DELETE",
+      });
+      mutate();
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
+  // function truncate(source, size) {
+  //   return source.length > size ? source.slice(0, size - 1) + "…" : source;
+  // }
 
+  // // if(styles)
+  // var res = truncate(post.content, post.content?.length);
 
-  if(session.status === 'authenticated'){
-
+  if (session.status === "authenticated") {
     return (
       <div className={styles.container}>
         <div className={styles.posts}>
           {isLoading
             ? "loading"
             : data?.map((post) => (
-              <div className={styles.post} key={post._id}>
-                  <div className={styles.imgContainer}>
-                    <Image src={post.img} alt="" width={200} height={100} />
+                <div className={styles.post} key={post._id}>
+                  <div className={styles.box}>
+                    <div className={styles.imgContainer}>
+                      <Image src={post.img} alt="" width={200} height={200} />
+                    </div>
+
+                    <div className={styles.details}>
+                      <div className={styles.postTitle}>{post.title} </div>
+                      <div className={styles.desc}>
+                        {post.desc.length > 50
+                          ? post.desc.substr(0, 50) + "..."
+                          : post.desc}
+                      </div>
+                      <div className={styles.content}>
+                        {post.content.length > 250
+                          ? post.content.substr(0, 250) + "..."
+                          : post.content}
+                        {/* {res} */}
+                      </div>
+                    </div>
                   </div>
-                  <h2 className={styles.postTitle}>{post.title}</h2>
-                  <span
+
+                  <div
                     className={styles.delete}
                     onClick={() => handleDelete(post._id)}
-                    >
-                    X
-                  </span>
+                  >
+                    x
+                  </div>
                 </div>
               ))}
         </div>
@@ -102,7 +123,7 @@ const Dashboard = () => {
             className={styles.textArea}
             cols="30"
             rows="10"
-            ></textarea>
+          ></textarea>
           <button className={styles.button}>Send</button>
         </form>
       </div>
